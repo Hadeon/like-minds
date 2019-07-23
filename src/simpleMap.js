3cb72684569f94e6747a1d631c5880d50cd0be7d
Hadeon <andersene909@gmail.com>
2019-07-23 12:45:05 -0700

    Added animations for pin; Coordinates set to Seattle;

--- src/simpleMap.js
+++ src/simpleMap.js
@@ -3,18 +3,28 @@ import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 
 const SimpleMap = (props) => {
-  const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
+  const getMapOptions = (maps) => {
+    return {
+      disableDefaultUI: true,
+      mapTypeControl: true,
+      streetViewControl: true,
+      styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }]
+    };
+  };
+
+  const [center, setCenter] = useState({lat: 47.6062, lng: -122.3321 });
   const [zoom, setZoom] = useState(11);
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'Your API Key here' }}
+        bootstrapURLKeys={{ key: 'Google Maps API Key here' }}
         defaultCenter={center}
         defaultZoom={zoom}
+        options={getMapOptions}
       >
         <Marker
-          lat={11.0168}
-          lng={76.9558}
+          lat={47.6062}
+          lng={-122.3321}
           name="Test Marker"
           color="blue"
         />
